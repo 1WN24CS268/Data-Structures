0e13736f556111b40396ec552bdac23a6f6bd1cf
1WN24CS268 <shravyaas.cs24@bmsce.ac.in>
2025-12-23 10:11:52 +0530

    Add files via upload

---
+++ DFS.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int G[20][20], visited[20], n;
+void dfs(int v)
+{
+    printf("%d ", v);
+    visited[v] = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (G[v][i] == 1 && visited[i] == 0)
+        {
+            dfs(i);
+        }
+    }
+}
+
+int main()
+{
+    int edges, u, v, start;
+    for (int i = 0; i < 20; i++)
+    {
+        visited[i] = 0;
+        for (int j = 0; j < 20; j++)
+        {
+            G[i][j] = 0;
+        }
+    }
+    printf("Enter number of vertices and edges: ");
+    scanf("%d %d", &n, &edges);
+    printf("Enter edges (u v):\n");
+    for (int i = 0; i < edges; i++)
+    {
+        scanf("%d %d", &u, &v);
+        G[u][v] = 1;
+        G[v][u] = 1;
+    }
+    printf("Enter start vertex: ");
+    scanf("%d", &start);
+    printf("DFS Traversal: ");
+    dfs(start);
+    return 0;
+}
